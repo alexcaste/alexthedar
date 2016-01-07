c7f3896b00b5ef3dddd4f6436485f342976e19b5
Alexandar Castaneda <alexthedar@users.noreply.github.com>
2016-01-07 10:22:43 -0800

    Update simpleness.js

--- js/simpleness.js
+++ js/simpleness.js
@@ -1,11 +1,10 @@
 $(function(){
 
-  // For Demo purposes only (show hover effect on mobile devices)
+
   [].slice.call( document.querySelectorAll('a[href="#"') ).forEach( function(el) {
     el.addEventListener( 'click', function(ev) { ev.preventDefault(); } );
   } );
-  //  scroll animated with wow.js
-  //  settings div wow
+
   wow = new WOW(
     {
       animateClass: 'animated fadeInUp',
@@ -14,7 +13,7 @@ $(function(){
       callback: function(box){ }
     }
   );
-  //  Go! div wow
+
   wow.init();
 
 });
